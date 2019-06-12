a5d17950d43223c059c238f4cac03f2b9816217d
vkondratiev94 <vkondratiev94@gmail.com>
2019-06-12 13:25:17 +0300

    simple nav example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSpring, animated } from 'react-spring'
+
 import Toggle from './Toggle'
+import Nav from './Nav'
+
 import logo from './logo.svg'
 import './App.css'
 
 const App = () => {
+  const [isNavOpen, setNavOpen] = useState(false)
+  const navAnimation = useSpring({
+    transform: isNavOpen
+      ? `translate3d(0, 0, 0) scale(1)`
+      : `translate3d(100%, 0%, 0) scale(0.6)`
+  })
   const fade = useSpring({
     from: {
       opacity: 0,
@@ -18,7 +27,11 @@ const App = () => {
     <animated.div className="App" style={fade}>
       <header className="App-header">
         <img src={logo} className="logo" />
-        <button className="menu-button">Menu</button>
+        <button
+          className="menu-button"
+          onClick={() => setNavOpen(!isNavOpen)}
+        >Menu</button>
+        <Nav style={navAnimation} />
       </header>
       <main>
         <Toggle />
