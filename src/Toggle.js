b056833624597e49a32805d9e06a211ba37ce983
vkondratiev94 <vkondratiev94@gmail.com>
2019-06-12 18:31:00 +0300

    transition with mounting and unmounting

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,25 +1,24 @@
 import React, { useState } from 'react'
-import { useSpring, animated } from 'react-spring'
+import { useTransition, animated } from 'react-spring'
 
 const Toggle = () => {
   const [isToggled, setIsToggled] = useState(false)
-  const { color, y } = useSpring({
-    // opacity: isToggled ? 1 : 0,
-    // fontSize: isToggled ? '2rem' : '5em',
-    // transform: isToggled ? 'translate3d(0, 0, 0)' : 'translate3d(0, -25px, 0)'
-    color: isToggled ? 'tomato' : 'green',
-    y: isToggled ? 0 : 1,
+  const transition = useTransition(isToggled, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 }
   })
 
   return (
     <div>
-      <animated.h1 style={{
-        transform: y.interpolate({
-          range: [0, .25, .50, .75, 1],
-          output: [0, -25, -50, -100, -50]
-        }).interpolate(y => `translate3d(0, ${y}px, 0)`),
-        color
-      }}>Hello</animated.h1>
+      {transition.map(({ item, key, props }) => 
+        item && (
+          <animated.h1
+            key={key}
+            style={props}
+          >Hello</animated.h1>
+        )
+      )}
       <button onClick={() => setIsToggled(!isToggled)}>Toggle</button>
     </div>
   )
