a716035e4e283130edc280abeba15cbf8a975177
vkondratiev94 <vkondratiev94@gmail.com>
2019-06-12 15:18:54 +0300

    emulating keyframes

--- src/Toggle.js
+++ src/Toggle.js
@@ -8,13 +8,16 @@ const Toggle = () => {
     // fontSize: isToggled ? '2rem' : '5em',
     // transform: isToggled ? 'translate3d(0, 0, 0)' : 'translate3d(0, -25px, 0)'
     color: isToggled ? 'tomato' : 'green',
-    y: isToggled ? 0 : -50,
+    y: isToggled ? 0 : 1,
   })
 
   return (
     <div>
       <animated.h1 style={{
-        transform: y.interpolate(y => `translate3d(0, ${y}px, 0)`),
+        transform: y.interpolate({
+          range: [0, .25, .50, .75, 1],
+          output: [0, -25, -50, -100, -50]
+        }).interpolate(y => `translate3d(0, ${y}px, 0)`),
         color
       }}>Hello</animated.h1>
       <button onClick={() => setIsToggled(!isToggled)}>Toggle</button>
