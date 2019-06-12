64415b3636dcf6735a4534768ebfac18ede45485
vkondratiev94 <vkondratiev94@gmail.com>
2019-06-12 12:29:13 +0300

    very basic animation finished

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useSpring, animated } from 'react-spring'
+import logo from './logo.svg'
+import './App.css'
+
+const App = () => {
+  const fade = useSpring({
+    from: {
+      opacity: 0,
+    },
+    to: {
+      opacity: 1
+    }
+  })
+
+  return (
+    <animated.div className="App" style={fade}>
+      <header className="App-header">
+        <img src={logo} className="logo" />
+        <button className="menu-button">Menu</button>
+      </header>
+    </animated.div>
+  )
+}
+
+export default App
